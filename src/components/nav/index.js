f6de9f836b0f99a73ef3ca1f50490fa745c173b8
Vinicius Pereira <viniciuspereiraa51@gmail.com>
2022-05-18 18:39:24 -0300

    lista_de_carros - REACT

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -9,15 +9,23 @@ const Nav = () => {
                     <div>
                         <a className="navbar-brand" href="/">
                             <img
-                                src="https://files.startupranking.com/startup/thumb/34374_e7a7b640a31ef8312bde02c3d31d9058b1e00bec_ws-work_l.jpg"
+                                src="https://i.pinimg.com/originals/b5/74/04/b574045f483218530f7f284802a03ec7.png"
                                 width="50px"
                                 height="40px"
                             ></img>
                         </a>
-                        <a style={{fontSize: "18px"}} className="navbar-brand" href="/">
+                        <a
+                            style={{ fontSize: "18px" }}
+                            className="navbar-brand"
+                            href="/"
+                        >
                             <strong>Adicionar carros</strong>
                         </a>
-                        <a style={{fontSize: "18px"}} className="navbar-brand" href="/home">
+                        <a
+                            style={{ fontSize: "18px" }}
+                            className="navbar-brand"
+                            href="/home"
+                        >
                             <strong>Lista de carros</strong>
                         </a>
                     </div>
